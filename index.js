381ebb309237d9902294825fbddaa0b9524119f5
Sebastian Valencia <sebastian.valencian@gmail.com>
2017-09-26 15:46:14 -0500

    Cambios en los test

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-exports.pruebaConcatenar = function(valor){
-    return valor + "prueba";
+exports.holaMundo = function(valor){
+    return valor + " Mundo";
 }
 
 exports.sumar = function(valor1, valor2){
     return valor1 + valor2;
 }
+
--- test/pruebasChaiAssert.js
+++ test/pruebasChaiAssert.js
@@ -4,14 +4,19 @@ var funcionAProbar = require('../index');
 
 //Se usa para definir bloques de pruebas relacionadas entre sí
 describe("Test con ASSERT interface de CHAI: ", function() {
-    describe("Testear la función pruebaSumar", function(){
+    describe("Testear la función holaMundo", function(){
         //cada it es una prueba, Las pruebas pueden estar divididas entre varios elementos
         //de tipo “describe”. Esto es útil si queremos diferenciar varios bloques 
         //de pruebas en un mismo fichero.
         it("Probar la respuesta con assert.equal(value, 'value')", function(){
-            result = funcionAProbar.pruebaConcatenar("texto de ");
-            assert.equal(result, "texto de prueba");
+            result = funcionAProbar.holaMundo("Hola");
+            assert.equal(result, "Hola Mundo");
         });
+        it("Probar que la respuesta sea una cadena", function(){
+            result = funcionAProbar.holaMundo("Hola");
+            assert.typeOf(result, 'string');
+        });
+
     });
 });
 
