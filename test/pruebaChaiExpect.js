93951e29b234b545f6ebdd135afed5e9941ca700
Sebastian Valencia <sebastian.valencian@gmail.com>
2017-10-01 21:49:08 -0500

    Prueba de numeros decimales

--- test/pruebaChaiExpect.js
+++ test/pruebaChaiExpect.js
@@ -5,12 +5,15 @@ var funcionAProbar = require('../index');
 describe("Test con EXPECT de función sumar: ", function(){
     it("Probar función sumar dos números enteros", function(){
         result = funcionAProbar.sumar(1,3);
-        expect(4).to.equal(4);
+        expect(result).to.equal(4);
     });
 
     it("Probar función sumar dos números negativos", function(){
         result = funcionAProbar.sumar(-1,-4);
-        expect(-5).to.equal(-5);
+        expect(result).to.equal(-5);
     });
-    
+    it("Probar función sumar dos números decimales", function(){
+        result = funcionAProbar.sumar(1.7,4.3);
+        expect(result).to.equal(6.0);
+    });
 });
